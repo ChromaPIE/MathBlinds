7a2940d7c975b84f2e8fcecdceb4d002d8391cc8
ChromaPIE <chromasetha@outlook.com>
2024-06-19 14:59:37 +0800

    init

---
+++ localization/en-us.lua
@@ -0,0 +1,131 @@
+return {
+	["descriptions"] = {
+		["Blind"] = {
+			["bl_mathbl_mahogany_millennium"] = {
+				["name"] = "Mahogany Millennium",
+				["text"] = {
+					"Non-base edition",
+					"jokers are disabled",
+				},
+			},
+			["bl_mathbl_approach"] = {
+				["name"] = "The Approach",
+				["text"] = {
+					"If chips are 90-100% of the blind",
+					"after play, increase blind by 25%",
+				},
+			},
+			["bl_mathbl_vanilla_void"] = {
+				["name"] = "Vanilla Void",
+				["text"] = {
+					"Cards give 0 chips",
+					"when scored",
+				},
+			},
+			["bl_mathbl_aggregate"] = {
+				["name"] = "The Aggregate",
+				["text"] = {
+					"#1# in #2# chance to",
+					"debuff each played card",
+				},
+			},
+			["bl_mathbl_cappuccino_circus"] = {
+				["name"] = "Cappuccino Circus",
+				["text"] = {
+					"Leftmost and rightmost jokers",
+					"are disabled each hand",
+				},
+			},
+			["bl_mathbl_witness"] = {
+				["name"] = "The Witness",
+				["text"] = {
+					"#1# ranks in deck",
+					"are debuffed",
+				},
+			},
+			["bl_mathbl_pure"] = {
+				["name"] = "The Pure",
+				["text"] = {
+					"Cards changed from or added to",
+					"the starting deck are debuffed",
+				},
+			},
+			["bl_mathbl_waiouru_wreath"] = {
+				["name"] = "Waiouru Wreath",
+				["text"] = {
+					"Play only one hand,",
+					"normal blind size",
+				},
+			},
+			["bl_mathbl_emerald_embedding"] = {
+				["name"] = "Emerald Embedding",
+				["text"] = {
+					"+5 hand size,",
+					"absurdly large blind",
+				},
+			},
+			["bl_mathbl_radical"] = {
+				["name"] = "The Radical",
+				["text"] = {
+					"All enhanced cards",
+					"are debuffed",
+				},
+			},
+			["bl_mathbl_norm"] = {
+				["name"] = "The Norm",
+				["text"] = {
+					"Ranks in played hands",
+					"can't exceed 21",
+				},
+			},
+			["bl_mathbl_bottom"] = {
+				["name"] = "The Bottom",
+				["text"] = {
+					"All low cards",
+					"are debuffed",
+				},
+			},
+			["bl_mathbl_floor"] = {
+				["name"] = "The Floor",
+				["text"] = {
+					"$0 blind reward",
+				},
+			},
+			["bl_mathbl_diamond_difference"] = {
+				["name"] = "Diamond Difference",
+				["text"] = {
+					"#1# in #2# cards shift",
+					"by 1 rank each draw",
+				},
+			},
+			["bl_mathbl_tip"] = {
+				["name"] = "The Tip",
+				["text"] = {
+					"Discarding a card",
+					"costs $1",
+				},
+			},
+			["bl_mathbl_bar"] = {
+				["name"] = "The Bar",
+				["text"] = {
+					"Cards held in hand",
+					"are debuffed",
+				},
+			},
+			["bl_mathbl_drop"] = {
+				["name"] = "The Drop",
+				["text"] = {
+					"-1 hand size",
+					"each hand played",
+				},
+			},
+			["bl_mathbl_infinite"] = {
+				["name"] = "The Infinite",
+				["text"] = {
+					"Cards can't",
+					"retrigger",
+				},
+			},
+		},
+	},
+}
